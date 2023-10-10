fd6be5c6ffdeb6e9bb008ebb2683f397bc2583b1
AYUSH PATEL <147489055+aayyusshh@users.noreply.github.com>
2023-10-10 10:05:12 +0000

    data

---
+++ basic/variable.js
@@ -0,0 +1,7 @@
+const accountID=12345
+let accountEmail="asas@gmail.com"
+var accountPass="09876"
+accountcity="patan"
+ 
+console.log(accountID);
+console.table([accountID,accountEmail,accountPass,accountcity]);
